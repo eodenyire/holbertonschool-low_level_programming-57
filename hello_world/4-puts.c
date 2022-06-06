be5df8698e17cc7a9b25a07b6bd84d925e7a370d
mousliiim <mouslimby77@gmail.com>
2022-06-07 01:22:45 +0200

    Exo4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/**
+* main - Main function for display message
+* Return: a 0
+*/
+
 int main(void)
-/* Test */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
