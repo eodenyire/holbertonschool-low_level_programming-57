0226609fc9ad7d11cdcf46cdb8385e5b43196563
mousliiim <mouslimby77@gmail.com>
2022-06-07 01:26:46 +0200

    Exo5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - Main function for display message with Printf
+* Return: a 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
