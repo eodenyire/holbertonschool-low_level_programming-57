4e66a38695db8e3973f96419f056f6991347f23c
mousliiim <mouslimby77@gmail.com>
2022-06-07 02:34:05 +0200

    Exo6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,8 +7,8 @@ int main(void)
 {
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of an int: %i byte(s)\n", sizeof(int));
-printf("Size of a long int: %li byte(s)\n", (int)sizeof(long));
-printf("Size of a long long int: %llu byte(s)\n", (int)(long)sizeof(long));
+printf("Size of a long int: %ld byte(s)\n", (int)sizeof(long));
+printf("Size of a long long int: %lld byte(s)\n", (long)(int)sizeof(long));
 printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
