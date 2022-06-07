0d1d6c1b288c3ac94004d287581a0ec40031e2ae
mousliiim <mouslimby77@gmail.com>
2022-06-08 01:05:48 +0200

    Print Alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Main function for print alphabet with putchar
+* Return: 0
+*/
+
+int main(void)
+{
+	char c='a';
+	while( c <= 'z')
+	putchar(c++);
+	putchar('\n');
+	return (0);
+}
