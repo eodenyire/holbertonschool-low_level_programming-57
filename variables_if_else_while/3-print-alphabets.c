a63793c8420094a47d691eb6b21e104a5ff52cb0
mousliiim <mouslimby77@gmail.com>
2022-06-08 01:17:13 +0200

    Print Alphabet with Maj

---
+++ variables_if_else_while/3-print-alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Main function for print alphabet with putchar
+* Return: 0
+*/
+
+int main(void)
+{
+	char c = 'a';
+	char d = 'A';
+
+	while (c <= 'z')
+{
+	putchar (c++);
+}
+	while(d <= 'Z')
+{
+	putchar(d++);
+}
+	putchar ('\n');
+	return (0);
+}
