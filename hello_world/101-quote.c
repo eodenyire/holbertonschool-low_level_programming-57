8aea7dcab6d3e4ec159fcb47bcf381594dbfd33a
mousliiim <mouslimby77@gmail.com>
2022-06-07 02:55:13 +0200

    Test101

---
+++ hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - Main function for display message with Printf
+* Return: a 1
+*/
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
