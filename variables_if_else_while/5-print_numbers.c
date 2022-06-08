1d1d3c319b13449897371c531339307d5e47646e
mousliiim <mouslimby77@gmail.com>
2022-06-08 02:27:40 +0200

    Print Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Main function for print alphabet with putchar
+* Return: 0
+*/
+
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+{
+	printf("%i", a);
+	a++;
+}
+	printf("\n");
+	return (0);
+}
