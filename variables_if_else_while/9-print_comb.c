f001fe0ad6029ecff6fa363d704b7838254fa953
mousliiim <mouslimby77@gmail.com>
2022-06-08 03:21:21 +0200

    Print Combinaison

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - program that prints all possible combinations of single-digit numbers
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	int j = 44;
+	int k = 32;
+
+	for (i = 48; i <= 57; i++)
+{
+	putchar(i);
+	putchar(j);
+	putchar(k);
+}
+	putchar('\n');
+	return (0);
+}
