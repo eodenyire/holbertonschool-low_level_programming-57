75c4853224151b7e0cb7ce4429e69b02e547a006
mousliiim <mouslimby77@gmail.com>
2022-06-08 02:48:57 +0200

    Print Number

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Main function for print digit number of base 10 start from 0 without variable of type char
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	int a = 48;
+
+	for (i = 0; i < 10; i++)
+{
+	putchar(a++);
+}
+	putchar('\n');
+	return (0);
+}
